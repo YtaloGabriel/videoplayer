8d2927d8e9e9248cdc2cd59790a17c0f01986f4c
ytalogabr <ytalogabr@gmail.com>
2023-01-04 07:32:28 -0300

    last changes

--- public/videosList.ts
+++ public/videosList.ts
@@ -31,14 +31,14 @@ export const videosList: IVideoInfo[] = [
   {
     id: 4,
     url: '/assets/videos/4.mp4',
-    thumb: 'https://i.ytimg.com/vi/3nQNiWdeH2Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAJPrsDWcK162o7Xu7l1iCELfUIYw',
+    thumb: 'https://i.ytimg.com/vi/oe70Uhjc_F4/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLBJpW_1Mg-m2r0FbHjV41stKGIFdw',
     title: 'FROM EARTH TO SPACE | Free HD VIDEO - NO COPYRIGHT',
     category: 'video'
   },
   {
     id: 5,
     url: '/assets/videos/5.mp4',
-    thumb: 'https://i.ytimg.com/vi/CmCIZ_aUAeQ/hqdefault.jpg?sqp=-oaymwEiCKgBEF5IWvKriqkDFQgBFQAAAAAYASUAAMhCPQCAokN4AQ==&rs=AOn4CLA_oFHIYHcZbF5AdNOYblOZnqUw-w',
+    thumb: 'https://i.ytimg.com/vi/4iC4f4S99S0/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDRG183HCJ8W7hpETndLRrDPqMfBw',
     title: 'Beauty Of Nature Time Lapse | Free HD Video - No Copyright',
     category: 'video'
   },
